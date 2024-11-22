4b64d82ffa084a8bbbfdbb5d8434783bb0fdf881
Jair-Rodriguez007 <jair123potrillo@gmail.com>
2024-11-22 01:58:07 -0600

    canbios en server

--- mi-backend/server.js
+++ mi-backend/server.js
@@ -81,6 +81,6 @@ app.post('/auth/register', async (req, res) => {
 });
 
 // Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
